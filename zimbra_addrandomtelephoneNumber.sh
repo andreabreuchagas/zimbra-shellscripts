3da7c59a82d72d22b0258d322e27e4045754c765
André da Cunha Pereira de Abreu Chagas <49037532+andreabreuchagas@users.noreply.github.com>
2019-03-28 14:02:15 -0300

    Update zimbra_addrandomtelephoneNumber.sh
    Just some visual stuff.

--- zimbra_addrandomtelephoneNumber.sh
+++ zimbra_addrandomtelephoneNumber.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-#==========================================================
+#
 # Zimbra Shell Script
 # zimbra_addrandomtelephoneNumber.sh
+#==========================================================
+# DESCRIPTION
 # Script to add a randon number (1 to 9999) on the
 # telephoneNumber field in zimbra ldap for all the accounts.
 #
@@ -12,20 +14,24 @@
 # Copyright (C) 2019 André Chagas <andreabreuchagas@gmail.com>
 # -
 # Version 1.0
-# 20190328 - Rev.00
+# 20190328 - Rev.01
 #
 
 echo "Obtaining zimbra accounts..."
 ZMLISTAMAIL="`su - zimbra -c 'zmprov -l gaa'`";
 count=0
+echo "Done."
 # For each account do:
-
+echo "Creating list of accounts with numbers"
 for email in $ZMLISTAMAIL; do
         count=$((count+1))
         #Set telephone to
         telephone=`seq -f "%04g" $count $count`
         executar[$count]="ma $email telephoneNumber $telephone"
 done
+echo "Done."
+echo "Applying telephone numbers to the accounts..."
 mkdir /tmp/zimbra_addrandomtelephoneNumber
 printf '%s\n' "${executar[@]}" > /tmp/zimbra_addrandomtelephoneNumber/listaaddtelephoneNumber.txt
 /opt/zimbra/bin/zmprov < /tmp/zimbra_addrandomtelephoneNumber/listaaddtelephoneNumber.txt
+echo "Done."
